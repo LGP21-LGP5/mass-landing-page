08ad093bf5a58282d0b397c2120d3d720ef8bc83
luispcunha <luispplmcunha@gmail.com>
2021-03-15 23:13:00 +0000

    Add administration to page

--- src/components/TeamGrid.js
+++ src/components/TeamGrid.js
@@ -1,15 +1,10 @@
 import * as React from "react"
 import ProfileCard from "./ProfileCard";
 
-
-
-
 const TeamGrid = () => {
-    //const team = [{ name: 'bernas670' }, { name: 'luispcunha' }]
-
     return (
         <div class="flex flex-row flex-wrap justify-around">
-            {[...Array(100).keys()].map((id) => <ProfileCard key={id} name='bernardo'></ProfileCard>)}
+            {[...Array(1000).keys()].map((id) => <ProfileCard key={id} name='bernardo'></ProfileCard>)}
         </div>
     );
 }
