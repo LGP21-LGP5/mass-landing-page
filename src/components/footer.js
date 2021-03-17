37cbe95a4d50bb8f52f806904f77cc2cf1bb36a6
João N. Matos <me@joaonmatos.com>
2021-03-17 11:44:57 +0000

    Implement footer

---
+++ src/components/footer.js
@@ -0,0 +1,21 @@
+import { Link } from "gatsby";
+import * as React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="flex flex-col sm:flex-row p-5 sm:py-3 bg-blue-600 sm:px-20">
+      <Link to="/faq" className="sm:flex-grow text-xl text-white font-light">
+        FAQ's
+      </Link>
+      <a className="text-xl text-white font-light sm:mx-8" href="#">
+        Instagram
+      </a>
+      <a className="text-xl text-white font-light sm:mx-8" href="#">
+        Facebook{" "}
+      </a>
+      <a className="text-xl text-white font-light sm:mx-8" href="#">
+        LinkedIn
+      </a>
+    </footer>
+  );
+}
