cb086ec8e60163159d50fb1852f7eded90b0b24a
luispcunha <luispplmcunha@gmail.com>
2021-03-17 23:04:19 +0000

    Fix graphql query

--- src/pages/meet.js
+++ src/pages/meet.js
@@ -1,18 +1,56 @@
 import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import ProfileCard from "../components/ProfileCard";
 import TeamGrid from "../components/TeamGrid";
 import TopMenu from "../components/top-menu";
-import useOperationsTeam from "../hooks/operations";
-import useMultimediaTeam from "../hooks/multimedia";
-import useServicesTeam from "../hooks/services";
 import logo from "../images/mass_icon.png" // Tell webpack this JS file uses this image
 
 
 const MeetUsPage = () => {
 
-  const operationsTeam = useOperationsTeam();
-  const multimediaTeam = useMultimediaTeam();
-  const servicesTeam = useServicesTeam();
+  const { operations, multimedia, services, ceo, cto, cbo, cmo } = useStaticQuery(graphql`
+    {
+      services: allCompanyMembersCsv(
+        filter: {Department: {eq: "Services"}, Added_Role: {ne: "CBO"}}
+      ) {
+        nodes {
+          Name
+        }
+      }
+      operations: allCompanyMembersCsv(
+        filter: {Department: {eq: "Operations"}, Added_Role: {ne: "CTO"}}
+      ) {
+        nodes {
+          Name
+          UP_Number
+        }
+      }
+      multimedia: allCompanyMembersCsv(
+        filter: {Department: {eq: "Multimedia"}, Added_Role: {ne: "CMO"}}
+      ) {
+        nodes {
+          Name
+          UP_Number
+        }
+      }
+      cto: companyMembersCsv(Added_Role: {eq: "CTO"}) {
+        UP_Number
+        Name
+      }
+      ceo: companyMembersCsv(Added_Role: {eq: "CEO"}) {
+        UP_Number
+        Name
+      }
+      cmo: companyMembersCsv(Added_Role: {eq: "CMO"}) {
+        UP_Number
+        Name
+      }
+      cbo: companyMembersCsv(Added_Role: {eq: "CBO"}) {
+        UP_Number
+        Name
+      }
+    }
+  `)
 
   return (
     <React.Fragment>
@@ -22,27 +60,27 @@ const MeetUsPage = () => {
           <p class='text-center'>Meet the team that makes our company stand out!</p>
           <div>
             <h1 class='text-center'>ADMINISTRATION</h1>
-            <ProfileCard name='Alexandre Silva' role='CEO' />
+            <ProfileCard name={ceo.Name} role='CEO' />
             <div class='flex flex-row justify-center'>
-              <ProfileCard name='Inês Ferraz' role='CMO' />
-              <ProfileCard name='João Matos' role='CTO' />
-              <ProfileCard name='Rafael Nogueira' role='CBO' />
+              <ProfileCard name={cmo.Name} role='CMO' />
+              <ProfileCard name={cto.name} role='CTO' />
+              <ProfileCard name={cbo.Name} role='CBO' />
             </div>
           </div>
           <div class="bg-gradient-to-t from-operations-blue-grad-from to-operations-blue-grad-to">
             <h1 class='text-center'>OPERATIONS</h1>
-            <ProfileCard name='João Matos' role='CTO' />
-            <TeamGrid team={operationsTeam}/>
+            <ProfileCard name={cto.name} role='CTO' />
+            <TeamGrid team={operations.nodes}/>
           </div>
           <div class="bg-gradient-to-b from-operations-blue-grad-from to-operations-blue-grad-to">
             <h1 class='text-center'>MULTIMEDIA</h1>
-            <ProfileCard name='Inês Ferraz' role='CMO' />
-            <TeamGrid team={multimediaTeam} />
+            <ProfileCard name={cmo.Name} role='CMO' />
+            <TeamGrid team={multimedia.nodes} />
           </div>
           <div class="bg-gradient-to-t from-operations-blue-grad-from to-operations-blue-grad-to">
             <h1 class='text-center'>SERVICES</h1>
-            <ProfileCard name='Rafael Nogueira' role='CBO' />
-            <TeamGrid team={servicesTeam} />
+            <ProfileCard name={cbo.Name} role='CBO' />
+            <TeamGrid team={services.nodes} />
           </div>
         </div>
     </React.Fragment>
