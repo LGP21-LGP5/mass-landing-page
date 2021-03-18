bce6ee5bb8e161673665041c0529a782999aa8d1
luispcunha <luispplmcunha@gmail.com>
2021-03-18 10:05:48 +0000

    Modify images appearance and replace class with className

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -3,7 +3,7 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 const ProfilePic = (props) => {
   return (
-    <GatsbyImage image={props.pic} alt={props.alt} />
+    <GatsbyImage image={props.pic} alt={props.alt} className='align-center rounded-full w-24 h-24 mx-auto' />
   );
 }
 
