4d793cf7b59c677ffe4b3282efcd3da01c5a5136
luispcunha <luispplmcunha@gmail.com>
2021-03-18 14:37:16 +0000

    Fix profile pics margin

--- src/pages/meet.js
+++ src/pages/meet.js
@@ -2,8 +2,7 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import ProfileCard from "../components/ProfileCard";
 import TeamGrid from "../components/TeamGrid";
-import TopMenu from "../components/top-menu";
-import logo from "../images/mass_icon.png" // Tell webpack this JS file uses this image
+import logo from "../images/mass_icon.png"
 import { getImage } from "gatsby-plugin-image"
 
 const MeetUsPage = () => {
@@ -13,6 +12,7 @@ const MeetUsPage = () => {
       name: Name
       up: UP_Number
       role: Added_Role
+      dept: Department
     }
 
     query {
@@ -67,31 +67,28 @@ const MeetUsPage = () => {
 
   return (
     <React.Fragment>
-      <TopMenu />
-        <div>
-          <img className='mx-auto mt-10 w-24' src={logo} alt='Mass company logo' />
-          <p className='text-center mt-10 mb-16 text-2xl'>Meet the team that makes our company stand out!</p>
-          <div className='mb-24'>
-            <h1 className='text-center font-bold text-3xl mb-16'>ADMINISTRATION</h1>
-            <ProfileCard member={ceo} pics={pictures} />
-            <TeamGrid team={[cbo, cto, cmo]} pics={pictures} />
-          </div>
-          <div className="bg-gradient-to-t from-operations-grad-from to-operations-grad-to pb-24">
-            <h1 className='text-center font-bold text-3xl mb-16 pt-24 text-operations'>OPERATIONS</h1>
-            <ProfileCard member={cto} pics={pictures} />
-            <TeamGrid team={operations.nodes} pics={pictures}/>
-          </div>
-          <div className="bg-gradient-to-b from-multimedia-grad-from to-multimedia-grad-to pb-24">
-            <h1 className='text-center font-bold text-3xl mb-16 pt-24 text-multimedia'>MULTIMEDIA</h1>
-            <ProfileCard member={cmo} pics={pictures} />
-            <TeamGrid team={multimedia.nodes} pics={pictures} />
-          </div>
-          <div className="bg-gradient-to-t from-operations-grad-from to-operations-grad-to pb-24">
-            <h1 className='text-center font-bold text-3xl mb-16 pt-24 text-operations'>SERVICES</h1>
-            <ProfileCard member={cbo} pics={pictures} />
-            <TeamGrid team={services.nodes} pics={pictures} />
-          </div>
-        </div>
+      <img className='mx-auto mt-10 w-24' src={logo} alt='Mass company logo' />
+      <p className='text-center mt-10 mb-16 text-2xl'>Meet the team that makes our company stand out!</p>
+      <div className='mb-24'>
+        <h1 className='text-center font-bold text-3xl mb-16'>ADMINISTRATION</h1>
+        <ProfileCard member={ceo} pics={pictures} showRole={true} />
+        <TeamGrid team={[cbo, cto, cmo]} pics={pictures} showRoles={true} />
+      </div>
+      <div className="bg-gradient-to-t from-operations-grad-from to-operations-grad-to pb-24">
+        <h1 className='text-center font-bold text-3xl mb-16 pt-24 text-operations'>OPERATIONS</h1>
+        <ProfileCard member={cto} pics={pictures} showRole={true} />
+        <TeamGrid team={operations.nodes} pics={pictures} showRoles={false} />
+      </div>
+      <div className="bg-gradient-to-b from-multimedia-grad-from to-multimedia-grad-to pb-24">
+        <h1 className='text-center font-bold text-3xl mb-16 pt-24 text-multimedia'>MULTIMEDIA</h1>
+        <ProfileCard member={cmo} pics={pictures} showRole={true}/>
+        <TeamGrid team={multimedia.nodes} pics={pictures} showRoles={false} />
+      </div>
+      <div className="bg-gradient-to-t from-operations-grad-from to-operations-grad-to pb-24">
+        <h1 className='text-center font-bold text-3xl mb-16 pt-24 text-operations'>SERVICES</h1>
+        <ProfileCard member={cbo} pics={pictures} showRole={true}/>
+        <TeamGrid team={services.nodes} pics={pictures} showRoles={false} />
+      </div>
     </React.Fragment>
   );
 }
