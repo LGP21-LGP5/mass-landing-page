f30a5512d3ad89cb7db76783b222e3cd7b3a5bf9
luispcunha <luispplmcunha@gmail.com>
2021-03-18 15:57:53 +0000

    Fix grid when team has less than 5 elements

--- src/components/TeamGrid.js
+++ src/components/TeamGrid.js
@@ -2,11 +2,13 @@ import * as React from "react"
 import ProfileCard from "./ProfileCard";
 
 const TeamGrid = (props) => {
-    const nCols = props.team.length >= 5 ? 5 : props.team.length
+    const nCols = props.team.length
 
     return (
-        <div className={`grid grid-cols-${nCols}`}>
+        <div className={`grid grid-cols-5 w-8/12 mx-auto`}>
+            {nCols === 3 && <div/>}
             {props.team.map((m, i) => <ProfileCard key={i} member={m} pics={props.pics} showRole={props.showRoles}></ProfileCard>)}
+            {nCols === 3 && <div/>}
         </div>
     );
 }
