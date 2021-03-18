135197408cc5ee328f3f3717a4fe15e260c16003
Bernardo Santos <bernardosantos670@gmail.com>
2021-03-18 20:17:42 +0000

    Created a layout component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,7 @@
 import * as React from "react";
-import TopMenu from "../components/top-menu";
-import Footer from "../components/footer";
+import Layout from "../components/layout";
 
-
-import logo from "../images/icon.png";
+// import logo from "../images/icon.png";
 import * as styles from "./contact.module.css";
 import { StaticImage } from "gatsby-plugin-image";
 
@@ -12,10 +10,15 @@ const ContactUsPage = () => {
 
 
     return (
-        <React.Fragment>
-            <TopMenu />
+        <Layout>
 
-            <img src={logo} alt="Mass logo" className="mx-auto w-24 mt-16 mb-20" />
+            <div className="mt-16 mb-20 flex justify-center">
+                <StaticImage
+                    className="w-24"
+                    src="../images/icon.png"
+                    alt="Mass Logo"
+                />
+            </div>
 
             <h1 className="text-center text-4xl mb-14">Get in touch with us!</h1>
 
@@ -44,30 +47,28 @@ const ContactUsPage = () => {
                 </form>
                 <div className="w-1/6 flex flex-col justify-end mr-32 ml-5">
                     <div className="flex flex-row justify-between mb-6">
-                        <StaticImage 
+                        <StaticImage
                             src="../images/facebook.png"
                             alt="Mass' Facebook"
                             height={40}
-                            />
-                        <StaticImage 
+                        />
+                        <StaticImage
                             src="../images/linkedin.png"
                             alt="Mass' LinkedIn"
                             height={40}
-                            />
-                        <StaticImage 
+                        />
+                        <StaticImage
                             src="../images/instagram.png"
                             alt="Mass' Instagram"
                             height={40}
-                            />
+                        />
                     </div>
                     <button className="bg-blue-600 text-white h-12  ">Send</button>
                 </div>
             </div>
 
 
-
-            <Footer />
-        </React.Fragment>
+        </Layout>
     )
 }
 
