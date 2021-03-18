bad10198c9647d7a4b830a79ca4dc7006ca791a1
luispcunha <luispplmcunha@gmail.com>
2021-03-18 09:42:59 +0000

    Load profile pictures

--- src/pages/meet.js
+++ src/pages/meet.js
@@ -4,83 +4,96 @@ import ProfileCard from "../components/ProfileCard";
 import TeamGrid from "../components/TeamGrid";
 import TopMenu from "../components/top-menu";
 import logo from "../images/mass_icon.png" // Tell webpack this JS file uses this image
-
+import { getImage } from "gatsby-plugin-image"
 
 const MeetUsPage = () => {
 
-  const { operations, multimedia, services, ceo, cto, cbo, cmo } = useStaticQuery(graphql`
-    {
+  const { operations, multimedia, services, ceo, cto, cbo, cmo, pictureFiles } = useStaticQuery(graphql`
+    fragment memberFields on CompanyMembersCsv {
+      name: Name
+      up: UP_Number
+      role: Added_Role
+    }
+
+    query {
       services: allCompanyMembersCsv(
         filter: {Department: {eq: "Services"}, Added_Role: {ne: "CBO"}}
       ) {
         nodes {
-          Name
+          ...memberFields
         }
       }
       operations: allCompanyMembersCsv(
         filter: {Department: {eq: "Operations"}, Added_Role: {ne: "CTO"}}
       ) {
         nodes {
-          Name
-          UP_Number
+          ...memberFields
         }
       }
       multimedia: allCompanyMembersCsv(
         filter: {Department: {eq: "Multimedia"}, Added_Role: {ne: "CMO"}}
       ) {
         nodes {
-          Name
-          UP_Number
+          ...memberFields
         }
       }
       cto: companyMembersCsv(Added_Role: {eq: "CTO"}) {
-        UP_Number
-        Name
+        ...memberFields
       }
       ceo: companyMembersCsv(Added_Role: {eq: "CEO"}) {
-        UP_Number
-        Name
+        ...memberFields
       }
       cmo: companyMembersCsv(Added_Role: {eq: "CMO"}) {
-        UP_Number
-        Name
+        ...memberFields
       }
       cbo: companyMembersCsv(Added_Role: {eq: "CBO"}) {
-        UP_Number
-        Name
+        ...memberFields
+      }
+      pictureFiles: allFile(
+        filter: {extension: {regex: "/png|jpg|jpeg/"}, name: {regex: "/(up.*)|default/"}}
+      ) {
+        nodes {
+          name
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   `)
+
+  const pictures = []
+  pictureFiles.nodes.forEach(pic => pictures[pic.name] = getImage(pic))
 
   return (
     <React.Fragment>
       <TopMenu />
         <div>
-          <img class='mx-auto w-24' src={logo}/>
+          <img class='mx-auto w-24' src={logo} alt='Mass company logo' />
           <p class='text-center'>Meet the team that makes our company stand out!</p>
           <div>
             <h1 class='text-center'>ADMINISTRATION</h1>
-            <ProfileCard name={ceo.Name} role='CEO' />
+            <ProfileCard member={ceo} pics={pictures} />
             <div class='flex flex-row justify-center'>
-              <ProfileCard name={cmo.Name} role='CMO' />
-              <ProfileCard name={cto.name} role='CTO' />
-              <ProfileCard name={cbo.Name} role='CBO' />
+              <ProfileCard member={cmo} pics={pictures} />
+              <ProfileCard member={cto} pics={pictures} />
+              <ProfileCard member={cbo} pics={pictures} />
             </div>
           </div>
           <div class="bg-gradient-to-t from-operations-blue-grad-from to-operations-blue-grad-to">
             <h1 class='text-center'>OPERATIONS</h1>
-            <ProfileCard name={cto.name} role='CTO' />
-            <TeamGrid team={operations.nodes}/>
+            <ProfileCard member={cto} pics={pictures} />
+            <TeamGrid team={operations.nodes} pics={pictures}/>
           </div>
           <div class="bg-gradient-to-b from-operations-blue-grad-from to-operations-blue-grad-to">
             <h1 class='text-center'>MULTIMEDIA</h1>
-            <ProfileCard name={cmo.Name} role='CMO' />
-            <TeamGrid team={multimedia.nodes} />
+            <ProfileCard member={cmo} pics={pictures} />
+            <TeamGrid team={multimedia.nodes} pics={pictures} />
           </div>
           <div class="bg-gradient-to-t from-operations-blue-grad-from to-operations-blue-grad-to">
             <h1 class='text-center'>SERVICES</h1>
-            <ProfileCard name={cbo.Name} role='CBO' />
-            <TeamGrid team={services.nodes} />
+            <ProfileCard member={cbo} pics={pictures} />
+            <TeamGrid team={services.nodes} pics={pictures} />
           </div>
         </div>
     </React.Fragment>
