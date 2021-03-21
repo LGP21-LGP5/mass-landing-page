1c744d10b728135be9f3a0bc7692791f637c4ec5
João Praça <jarpraca@gmail.com>
2021-03-21 02:13:20 +0000

    Improve team grid responsiveness

--- src/components/TeamGrid.js
+++ src/components/TeamGrid.js
@@ -5,7 +5,7 @@ const TeamGrid = (props) => {
     const nCols = props.team.length
 
     return (
-        <div className={`grid grid-cols-5 w-8/12 mx-auto`}>
+        <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-8/12 mx-auto`}>
             {nCols === 3 && <div/>}
             {props.team.map((m, i) => <ProfileCard key={i} member={m} pics={props.pics} showRole={props.showRoles}></ProfileCard>)}
             {nCols === 3 && <div/>}
