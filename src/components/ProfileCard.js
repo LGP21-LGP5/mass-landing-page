76b153657c41942d2216dfc41cf1356dd90337b4
João Praça <jarpraca@gmail.com>
2021-03-21 14:56:23 +0000

    Delete comment

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -8,7 +8,7 @@ const ProfileCard = (props) => {
   return (
     <div className='flex flex-col'>
       <ProfilePic pic={picture} alt={`Profile picture of ${member.name}`} className='mb-6 mt-6'/>
-      {props.showRole && /*member.role.match(/C.O/) &&*/ <div className='text-center font-poppins-bold text-xl mb-2'>{member.role}</div>}
+      {props.showRole && <div className='text-center font-poppins-bold text-xl mb-2'>{member.role}</div>}
       <div className='text-center font-poppins text-lg'>{member.name}</div>
       <div className='text-center font-poppins mt-0'>LinkedIn</div>
     </div>
