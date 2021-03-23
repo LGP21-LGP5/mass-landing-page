8da61888884d56535a5941bc5b993a49b66130cd
luispcunha <luispplmcunha@gmail.com>
2021-03-23 16:45:03 +0000

    Add responsivity

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -6,11 +6,10 @@ const ProfileCard = (props) => {
   const picture = props.pics[props.member.up] || props.pics['default']
 
   return (
-    <div className='flex flex-col'>
-      <ProfilePic pic={picture} alt={`Profile picture of ${member.name}`} className='mb-6 mt-6'/>
+    <div className='flex flex-col mx-5'>
+      <ProfilePic pic={picture} alt={`Profile picture of ${member.name}`} className='mb-2 md:mb-6 mt-6'/>
       {props.showRole && member.role.match(/C.O/) && <div className='text-center font-bold text-2xl mb-2'>{member.role}</div>}
       <div className='text-center text-xl'>{member.name}</div>
-      <div className='text-center mt-0'>LinkedIn</div>
     </div>
   );
 }
