0a445454fcf633e1011362522735dc0b3cf22d12
João N. Matos <me@joaonmatos.com>
2021-03-25 22:16:48 +0000

    Change NOSE to Fashable

--- src/pages/projects/fashable.js
+++ src/pages/projects/fashable.js
@@ -7,7 +7,7 @@ import TeamGrid from "../../components/TeamGrid";
 
 import icon from "../../images/icon.png";
 
-const NosePage = () => {
+const FashablePage = () => {
   const { team, teamLeader, pictureFiles } = useStaticQuery(graphql`
     fragment memberFields2 on CompanyMembersCsv {
       name: Name
@@ -31,7 +31,10 @@ const NosePage = () => {
         ...memberFields2
       }
       pictureFiles: allFile(
-        filter: {extension: {regex: "/png|jpg|jpeg/"}, name: {regex: "/((up)?.*)|default/"}}
+        filter: {
+          extension: { regex: "/png|jpg|jpeg/" }
+          name: { regex: "/((up)?.*)|default/" }
+        }
       ) {
         nodes {
           name
@@ -44,21 +47,21 @@ const NosePage = () => {
   `);
 
   const pictures = [];
-  pictureFiles.nodes.forEach(pic => {
+  pictureFiles.nodes.forEach((pic) => {
     const name = pic.name.match(/up.*|default/) ? pic.name : `up${pic.name}`;
     pictures[name] = getImage(pic);
-  })
+  });
 
   return (
     <Layout>
       <img className="mx-auto mt-10 w-24" src={icon} alt="Mass company logo" />
       <div className="mt-20 mb-10 flex flex-col justify-center">
-        <p className="mx-auto font-poppins-bold text-4xl">NOSE</p>
+        <p className="mx-auto font-poppins-bold text-4xl">Fashable</p>
       </div>
       <div className="mb-10 mx-10 sm:mx-20 xl:mx-40 2xl:mx-80">
         <p className="mx-auto font-poppins-bold text-center text-2xl">About</p>
         <p className="mx-auto font-poppins text-center text-gray-800 text-lg mt-2">
-          NOSE is an e-commerce platform proposed by XNFY Lab and being
+          Fashable is an e-commerce platform proposed by XNFY Lab and being
           developed by Mass. It is an online intelligent marketplace looking to
           improve the democratization of the marketplace space and focused on
           providing the utmost realistic experience for those willing to try a
@@ -69,13 +72,13 @@ const NosePage = () => {
           <br />
           <br />
           Customers are shown products filtered based on their personal
-          preferences and interactions within the platform. NOSE also allows
+          preferences and interactions within the platform. Fashable also allows
           small retailers to gain their space in the market and compete on fair
           terms against other well-known companies through filiation programs.
           <br />
           <br />
           Unlike other solutions that only show catalogs for specific items or
-          single branded, NOSE creates a unique experience that simulates a
+          single branded, Fashable creates a unique experience that simulates a
           truly shopping experience that will revolutionize online shopping as
           we know it.
         </p>
@@ -103,4 +106,4 @@ const NosePage = () => {
   );
 };
 
-export default NosePage;
+export default FashablePage;
