f89a467c748ad2a4abbd50bb8577b4f0bd9e2970
luispcunha <luispplmcunha@gmail.com>
2021-03-25 16:59:35 +0000

    Use Layout component instead of TopBar and Footer in project pages

--- src/pages/projects/signalcheck.js
+++ src/pages/projects/signalcheck.js
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage, getImage } from "gatsby-plugin-image";
-import TopMenu from "../../components/top-menu";
-import Footer from "../../components/footer";
+import Layout from "../../components/layout";
 import ProfileCard from "../../components/ProfileCard";
 import TeamGrid from "../../components/TeamGrid";
 
@@ -51,8 +50,7 @@ const SignalCheckPage = () => {
   })
 
   return (
-    <React.Fragment>
-      <TopMenu />
+    <Layout>
       <img className="mx-auto mt-10 w-24" src={icon} alt="Mass company logo" />
       <div className="mt-20 mb-10 flex flex-col justify-center">
         <p className="mx-auto font-poppins-bold text-4xl">SignalCheck</p>
@@ -92,8 +90,7 @@ const SignalCheckPage = () => {
           />
         </a>
       </div>
-      <Footer />
-    </React.Fragment>
+    </Layout>
   );
 };
 
