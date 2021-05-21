8aac0273e05fda032f60e8484f5d97965f922e13
João N. Matos <me@joaonmatos.com>
2021-05-21 14:03:50 +0100

    Enlarge project logos and set navbar bg to white

--- src/pages/projects/efeupinho.js
+++ src/pages/projects/efeupinho.js
@@ -54,7 +54,11 @@ const EfeupinhoPage = () => {
 
   return (
     <Layout>
-      <img className="mx-auto mt-10 w-24" src={efeupinho} alt="eFeupinho logo" />
+      <img
+        className="mx-auto mt-10 w-72"
+        src={efeupinho}
+        alt="eFeupinho logo"
+      />
       <div className="mt-20 mb-10 flex flex-col justify-center">
         <p className="mx-auto font-poppins-bold text-4xl">eFeupinho</p>
       </div>
